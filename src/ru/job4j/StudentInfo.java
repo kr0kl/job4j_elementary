b5655eed39703a19e82908742d2bf50f903b2548
kr0kl <kr0kl@yandex.ru>
2021-01-15 22:37:00 +0300

    3.3. Разбор первой программы. [#450956]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Petr Arsentev");
+        System.out.println("Petr Arsentev");
+
+    }
+}
