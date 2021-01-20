e7f65dc1ad1c524e31d6102286637290fd4221bc
kr0kl <kr0kl@yandex.ru>
2021-01-20 11:33:49 +0300

    4.6.1. Switch [#450892]

---
+++ test/ru/job4j/condition/SwitchWeekTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SwitchWeekTest {
+
+    @Test
+
+        public void whenDayFriday() {
+            String name = SwitchWeek.nameOfDay(5);
+            assertThat(name, is("Friday"));
+    }
+    @Test
+
+    public void whenDayError() {
+        String name = SwitchWeek.nameOfDay(8);
+        assertThat(name, is("Ошибка"));
+    }
+}
