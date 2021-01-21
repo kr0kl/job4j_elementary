0a905e968729a5debe930b514b33aac6110163ec
kr0kl <kr0kl@yandex.ru>
2021-01-21 13:47:06 +0300

    sqArea

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(int p, int k) {
         int h = p / (2 * (k + 1));
-        int l = h + k;
+        int l = h * k;
 
         return l * h;
     }
