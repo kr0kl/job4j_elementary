a598b4ad9287973e9812f97123cd3fc60921b444
kr0kl <kr0kl@yandex.ru>
2021-01-26 22:02:10 +0300

    6.7.4. Выигрышные комбинации в сокобан [#450907]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -22,11 +22,23 @@ public class MatrixCheck {
         }
         return result;
     }
+
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int i = 0; i < board.length; i++) {
             rsl[i] = board[i][i];
         }
         return rsl;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if ((monoHorizontal(board, i) || monoVertical(board, i)) == true) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
